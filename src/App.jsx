44090a7b6da3049de501c12ec83d774262f497d5
erwan <erwangodelle@gmail.com>
2026-05-20 16:38:17 +0200

    add: section Projects avec les 3 cartes (alternance img gauche/droite)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Header from './components/Header'
 import Intro from './components/Intro'
-import ProjectCard from './components/ProjectCard'
-import SectionTitle from './components/SectionTitle'
+import Projects from './components/Projects'
 import yellowBg from './assets/yellow-bg.png'
 import heroWoman from './assets/hero-woman.png'
-import project1 from './assets/project-1.png'
 
 function App() {
   return (
@@ -35,18 +33,7 @@ function App() {
 
       <main>
         <Intro />
-
-        <section className="max-w-248 mx-auto px-6 py-16">
-          <SectionTitle>Projects</SectionTitle>
-
-          <div className="mt-10">
-            <ProjectCard
-              name="Project Name"
-              description="I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."
-              image={project1}
-            />
-          </div>
-        </section>
+        <Projects />
       </main>
     </div>
   )
---
+++ src/components/Projects.jsx
@@ -0,0 +1,41 @@
+import ProjectCard from './ProjectCard'
+import SectionTitle from './SectionTitle'
+import project1 from '../assets/project-1.png'
+import project2 from '../assets/project-2.png'
+import project3 from '../assets/project-3.png'
+
+const projects = [
+  {
+    name: 'Project Name',
+    description:
+      'I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.',
+    image: project1,
+  },
+  {
+    name: 'Project Name',
+    description:
+      'What was your role, your deliverables, if the project was personal, freelancing.',
+    image: project2,
+    reverse: true,
+  },
+  {
+    name: 'Project Name',
+    description:
+      'You can also add in this description the type of the project, if it was for web, mobile, electron.',
+    image: project3,
+  },
+]
+
+export default function Projects() {
+  return (
+    <section id="projects" className="max-w-248 mx-auto px-6 py-16">
+      <SectionTitle>Projects</SectionTitle>
+
+      <div className="mt-10 flex flex-col gap-8">
+        {projects.map((project, i) => (
+          <ProjectCard key={i} {...project} />
+        ))}
+      </div>
+    </section>
+  )
+}
