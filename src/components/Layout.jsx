7af2ffbf183df8a0d1fba7e2cee3bfd11ee43b3a
erwan <erwangodelle@gmail.com>
2026-05-22 00:03:02 +0200

    style: design tokens (Google Fonts + couleurs Figma)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 export default function Layout() {
   return (
-    <div className="relative flex min-h-screen flex-col bg-white">
+    <div className="relative flex min-h-screen flex-col bg-page">
       <Header />
 
       <main className="flex-1">
