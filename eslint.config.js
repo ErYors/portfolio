88cd1f842fc4229b1ab688e51c99f126c1797ab8
erwan <erwangodelle@gmail.com>
2026-06-02 22:38:18 +0200

    build: mise en place du toolchain TypeScript strict

--- eslint.config.js
+++ eslint.config.js
@@ -4,27 +4,42 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+
+const sharedExtends = [
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
+  jsxA11y.flatConfigs.recommended,
+]
+
+const sharedLanguageOptions = {
+  globals: globals.browser,
+  parserOptions: { ecmaFeatures: { jsx: true } },
+}
+
+const sharedSettings = {
+  react: { version: '19.2' },
+}
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      react.configs.flat.recommended,
-      react.configs.flat['jsx-runtime'],
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      jsxA11y.flatConfigs.recommended,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+    extends: [js.configs.recommended, ...sharedExtends],
+    languageOptions: sharedLanguageOptions,
+    settings: sharedSettings,
+    rules: {
+      'react/prop-types': 'off',
     },
-    settings: {
-      react: { version: '19.2' },
-    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...sharedExtends],
+    languageOptions: sharedLanguageOptions,
+    settings: sharedSettings,
     rules: {
       'react/prop-types': 'off',
     },
